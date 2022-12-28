2114f0167f33536a9f0ca1790e45006dc3877571
Albert Shen <albertshen1206@gmail.com>
2022-12-29 01:18:23 +0800

    up

---
+++ Plugin/RuleCollection.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Copyright © PHP Digital, Inc. All rights reserved.
+ */
+namespace AlbertMage\Quote\Plugin;
+
+use Magento\Quote\Model\Quote\Address;
+
+/**
+ * Rule Collection
+ */
+class RuleCollection
+{
+
+    /**
+     *
+     */
+    public function __construct(
+    ) {
+    }
+
+    /**
+     * Filter collection by specified website, customer group, coupon code, date.
+     * Filter collection to use only active rules.
+     * Involved sorting by sort_order column.
+     *
+     * @param Magento\SalesRule\Model\ResourceModel\Rule\Collection $collection
+     * @param Magento\SalesRule\Model\ResourceModel\Rule\Collection $result
+     * @param int $websiteId
+     * @param int $customerGroupId
+     * @param string $couponCode
+     * @param string|null $now
+     * @param Address $address allow extensions to further filter out rules based on quote address
+     * @throws \Zend_Db_Select_Exception
+     * @return Magento\SalesRule\Model\ResourceModel\Rule\Collection
+     */
+    public function afterSetValidationFilter(
+        \Magento\SalesRule\Model\ResourceModel\Rule\Collection $collection,
+        $result,
+        $websiteId,
+        $customerGroupId,
+        $couponCode = '',
+        $now = null,
+        Address $address = null
+    ) {
+        $collection->addFieldToFilter('rule_id', ['neq' => 3]);
+        return $result;
+    }
+
+}
